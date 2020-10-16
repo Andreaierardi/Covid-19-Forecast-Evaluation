7ff4f6d63c607a0ac8bcd38e579369ccdeef179d
Andreaierardi <andreierardi@gmail.com>
2020-10-16 13:05:14 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['forecast-evaluation.herokuapp.com','localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app' 
+    'app'
 ]
 
 MIDDLEWARE = [
