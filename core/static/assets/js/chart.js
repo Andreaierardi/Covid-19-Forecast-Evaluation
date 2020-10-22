752bdb69acbad0d0759db9530a38d0e77c7cce9e
Andreaierardi <andreierardi@gmail.com>
2020-10-22 16:39:48 +0200

    Update chart.js

--- core/static/assets/js/chart.js
+++ core/static/assets/js/chart.js
@@ -9,10 +9,7 @@ $(function() {
     datasets: [{
       label: '# of Votes',
       data: [10, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-
-      ],
+      backgroundColor: [  'rgba(255, 99, 132, 0.2)'   ],
       borderColor: [
         'rgba(255,99,132,1)',
         'rgba(54, 162, 235, 1)',
@@ -87,16 +84,16 @@ $(function() {
     datasets: [{
       data: [30, 40, 30],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.5)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)'
 
       ],
       borderColor: [
         'rgba(255,99,132,1)',
         'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
+        'rgba(255, 206, 86, 1)'
+
       ],
     }],
 
@@ -119,18 +116,8 @@ $(function() {
     datasets: [{
       label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
+      backgroundColor: [  'rgba(255, 99, 132, 0.2)'    ],
+      borderColor: [        'rgba(255,99,132,1)'      ],
       borderWidth: 1,
       fill: true, // 3: no fill
     }]
