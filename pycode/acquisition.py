f18130b081b88f64cfcab38ec5b40aa94968a189
Fabio Caironi <60004596+fabio130497@users.noreply.github.com>
2020-10-30 16:15:15 +0100

    first update

--- pycode/acquisition.py
+++ pycode/acquisition.py
@@ -7,24 +7,28 @@ state = ['US', 'California', 'Florida', 'New York', 'Texas']
 w0 = 5
 loss = ['MSE', 'MAE', 'MAPE', 'LINEX']
 
-raw_data = pd.read_csv("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Deaths.csv")
+## True data: begins with "T"
+# True Deaths: TD
+TD = pd.read_csv("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Deaths.csv")
+TStates = TD.location_name.unique()
 
-raw_data.columns
-
-i = 0
-
-daily_s = pd.Series(raw_data[raw_data['location_name'] == state[i] ].iloc[:,3].values,
-              index = pd.to_datetime(raw_data[raw_data['location_name'] == state[i] ].iloc[:,0].values, format="%Y-%m-%d")
+# True Deaths Series getter
+def getTDS(state, aggregate = True):
+     out = pd.Series(TD[TD['location_name'] == state].iloc[:,3].values,
+                    index = pd.to_datetime(TD[TD['location_name'] == state].iloc[:,0].values, format="%Y-%m-%d"),
+                    name = state + ": Cumulative deaths"
                    )
+     if(aggregate):
+          out = out[out.index.weekday == 5]
+     return(out)
 
-# shift series to first non-zero occurence
-daily_s = daily_s[daily_s>0]
+# # shift series to first non-zero occurence
+# daily_s = daily_s[daily_s>0]
+# # switch aggregation range to weekly (every Saturday)
+# D = daily_s[daily_s.index.weekday == 5]
 
-# switch aggregation range to weekly (every Saturday)
-D = daily_s[daily_s.index.weekday == 5]
 
-
-## Forecast data ##
+## Forecast data: begins with "F"
 # Forecasted deaths: FD
 FD = pd.read_csv('https://www.cdc.gov/coronavirus/2019-ncov/covid-data/files/2020-10-19-model-data.csv')
 
