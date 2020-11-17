0c354956dcc58b4ac846017b18599e889b10b6bd
Fabio Caironi <60004596+fabio130497@users.noreply.github.com>
2020-11-17 19:24:15 +0100

    updated acquisition

--- pycode/acquisition.py
+++ pycode/acquisition.py
@@ -2,13 +2,104 @@ import numpy as np
 import pandas as pd
 from datetime import date
 
-## Real data: begins with "R"
-# Real Deaths: RD
-RD = pd.read_csv("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Deaths.csv")
-# Real Cases: RC
-RC = pd.read_csv("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Cases.csv")
+### NOTE ON LOCATION NAMES ###
+# - Location names are contained in the column named 'location_name' of each dataset
+# - Datasets FC, RC, RD initially contained series of all states and all counties. 
+# - forecast data's string for national location is 'National'. 
+# - Real data's string for national location is 'US'.
+# - Previously selected locations (and therefore to be used) are contained in the list named 'states'.
 
-Rstates = RD.location_name.unique()
+# Define the areas of interest 
+states = ['US', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+              'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+              'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+              'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+              'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+              'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+              'West Virginia', 'Wisconsin', 'Wyoming', 'District of Columbia', 'Puerto Rico', 'Guam', 'Virgin Islands',
+              'Northern Mariana Islands', 'American Samoa']
+
+## Forecast data: begins with "F"
+# Forecasted cases: FC
+FC = pd.read_csv("https://www.cdc.gov/coronavirus/2019-ncov/downloads/cases-updates/2020-10-19-all-forecasted-cases-model-data.csv")[lambda x: x.location_name.isin(states+['National'])]
+# Forecasted deaths: FD
+FD = pd.read_csv('https://www.cdc.gov/coronavirus/2019-ncov/covid-data/files/2020-10-19-model-data.csv')[lambda x: x.location_name.isin(states+['National'])]
+
+# Define unique lists
+Fmodels = list(np.unique(np.concatenate((FC.model.values, FD.model.values))))
+Fstates = list(np.unique(np.concatenate((FC.location_name.values, FD.location_name.values))))
+Fdates = list(np.unique(np.concatenate((FC.forecast_date.values, FD.forecast_date.values))))
+# [i in states for i in Fstates]
+
+# Forecast Series getter
+def getFS(type, model, state, Fdate):
+    """Gets the forecasted deaths series by model, state and forecast date
+
+    Parameters
+    ----------
+    type : str
+        'C' for cumulative cases. 
+        'D' for cumulative deaths.
+    model : str
+        The model of the forecast
+    state : str
+        The target state of the forecast
+    Fdate : str or datetime
+        The date when the forecast was performed. If a string, provide the format '%Y-%m-%d'.
+
+    Returns
+    -------
+    pandas.DataFrame
+        a data frame containing 5 series:
+           - point series
+           - 2.5% quantile
+           - 25% quantile
+           - 75% quantile
+           - 97.5% quantile
+        Indexes are of class pandas.DatetimeIndex.
+    """
+    if(state == 'US'):
+        state = 'National'
+    if(type == 'C'):
+        out = FC[(FC.model == model) & (FC.location_name == state) & (FC.forecast_date == Fdate)] 
+    elif(type == 'D'):
+        out = FD[(FD.model == model) & (FD.location_name == state) & (FD.forecast_date == Fdate) & FD.target.apply(str.endswith, args=('cum death',0))]
+    else:
+        return None
+    if( out.empty ):
+        return None
+    out = pd.DataFrame(out.iloc[:,-5:].values,
+                columns = out.columns[-5:],
+                index = pd.to_datetime(out.iloc[:,3], format="%Y-%m-%d")
+                   )
+     
+    return out
+     
+# Example: getFS('C', Fmodels[40], Fstates[0], Fdates[0])
+# Example: getFS('D', Fmodels[0], Fstates[0], Fdates[0]) is None
+
+
+
+
+
+
+
+
+
+
+
+#=================================================================================================================#
+## Real data: begins with "R"
+# Retrieve only the States (raw data also contains counties)
+
+# Real Deaths: RD
+RD = pd.read_csv("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Deaths.csv")[lambda x: x.location_name.isin(states)]
+# Real Cases: RC
+RC = pd.read_csv("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Cases.csv")[lambda x: x.location_name.isin(states)]
+
+# Name of all
+Rstates = list(np.unique(np.concatenate((RC.location_name.values, RD.location_name.values))))
+
 
 # Real Series getter
 def getRS(type, state, aggregateOn = 5):
@@ -39,78 +130,13 @@ def getRS(type, state, aggregateOn = 5):
                     index = pd.to_datetime(RD[RD['location_name'] == state].iloc[:,0].values, format="%Y-%m-%d"),
                     name = state + ": Cumulative deaths"
         )
-        if(aggregateOn is not False):
-            out = out[out.index.weekday == aggregateOn]
+    if(aggregateOn is not False):
+        out = out[out.index.weekday == aggregateOn]
 
     return(out)
 
-# Example: getRS('D',Rstates[1])
+# Example: getRS('D',states[0])
 
-# # shift series to first non-zero occurence
-# daily_s = daily_s[daily_s>0]
-# # switch aggregation range to weekly (every Saturday)
-# D = daily_s[daily_s.index.weekday == 5]
-
-
-## Forecast data: begins with "F"
-# Forecasted cases: FC
-FC = pd.read_csv("https://www.cdc.gov/coronavirus/2019-ncov/downloads/cases-updates/2020-10-19-all-forecasted-cases-model-data.csv")
-# Forecasted deaths: FD
-FD = pd.read_csv('https://www.cdc.gov/coronavirus/2019-ncov/covid-data/files/2020-10-19-model-data.csv')
-
-Fmodels = FD.model.unique()
-Fstates = FD.location_name.unique()
-
-# Forecast Series getter
-def getFS(type, model, state, Fdate):
-    """Gets the forecasted deaths series by model, state and forecast date
-
-    Parameters
-    ----------
-    type : str
-        'C' for cumulative cases. 
-        'D' for cumulative deaths.
-    model : str
-        The model of the forecast
-    state : str
-        The target state of the forecast
-    Fdate : str or datetime
-        The date when the forecast was performed. If a string, provide the format '%Y-%m-%d'.
-
-    Returns
-    -------
-    pandas.DataFrame
-        a data frame containing 5 series:
-           - point series
-           - 2.5% quantile
-           - 25% quantile
-           - 75% quantile
-           - 97.5% quantile
-        Indexes are of class pandas.DatetimeIndex.
-    """
-    if(type == 'C'):
-        out = FC[(FC.model == model) & (FC.location_name == state) & (FC.forecast_date == Fdate)] 
-    elif(type == 'D'):
-        out = FD[(FD.model == model) & (FD.location_name == state) & (FD.forecast_date == Fdate) & FD.target.apply(str.endswith, args=('cum death',0))]
-    else:
-        return None
-    if( out.empty ):
-        return None
-    out = pd.DataFrame(out.iloc[:,-5:].values,
-                columns = out.columns[-5:],
-                index = pd.to_datetime(out.iloc[:,3], format="%Y-%m-%d")
-                   )
-     
-    return out
-     
-# Example: getFS('C', Fmodels[1], Fstates[1], FD.forecast_date[1])
-
-
-
-
-# prova1 = pd.ExcelFile('Matlab to python/data_models-Florida.xlsx')
-
-# prova1.sheet_names
-
-# prova2 = prova1.parse('Ensamble')
-# prova2
+# Example: aa = getRS('D',states[0])
+#          bb = getFS('D', Fmodels[0], states[0], Fdates[0])
+#          bb.index.isin(aa.index)
