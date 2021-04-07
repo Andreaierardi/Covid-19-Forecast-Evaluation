ae494dc84de37ced25b658aae33db4d2e12408c7
andreaierardi <andreierardi@gmail.com>
2021-04-07 21:29:47 +0200

    reset

--- getters.py
+++ getters.py
@@ -29,8 +29,8 @@ def decompress_pickle(file):
  data = cPickle.load(data)
  return data
 
-#corr_dict = decompress_pickle('data/unique_lists/corr_dict.pkl')
-corr_dict ={}
+corr_dict = decompress_pickle('data/unique_lists/corr_dict.pkl')
+
 quantiles = (0.99, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.01, 0.975, 0.025)
 
 models = list(pd.read_csv("data/unique_lists/models.csv")['models'])
